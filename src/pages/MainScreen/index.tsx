8738043399ea48d0d0c5f0fa4876253e369685d3
GlennJong <zaku0225@gmail.com>
2025-08-08 17:09:31 +0000

    Add rolling ball.

--- src/pages/MainScreen/index.tsx
+++ src/pages/MainScreen/index.tsx
@@ -3,10 +3,27 @@ import './style.css'
 import Chatroom from "@/components/Chatroom";
 import TodoList from "@/components/TodoList";
 import Timer from "@/components/Timer";
+import { useEffect, useRef } from "react";
+import { Ball } from "./RollingBall";
 
 function MainScreen() {
+  const ballRef = useRef<Ball>(null);
+
+  useEffect(() => {
+    if (!ballRef.current) {
+      ballRef.current = new Ball(200, 200);
+    }
+    return () => {
+      ballRef.current?.destroy();
+      ballRef.current = null;
+    }
+  }, []);
+  
   return (
     <div className="main-screen">
+      <button onClick={() => ballRef.current?.left(100)}>left</button>
+      <button onClick={() => ballRef.current?.right(100)}>right</button>
+      <button onClick={() => ballRef.current?.leftUp(100)}>leftUp</button>
       <div className="top">
         <Timer />
       </div>
